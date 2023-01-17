ddcdd000ebf1078279c61813ca55569eae90cb50
Malcolm Miller <entid@protonmail.com>
2023-01-17 11:59:36 +1100

    simplified contract

--- PiggyBank.sol
+++ PiggyBank.sol
@@ -6,39 +6,33 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v4.x/co
 
 
 contract PiggyBank is Ownable {
+
     // mapping to track depositors and amount
     mapping(address => uint) public depositors;
-    //mapping to track the time a deposit is made and by whom
-    mapping(uint => address) public timeDeposited;
+    
 
-// this is an expanded mapping consolidating the 2 previous mappings
-// an address maps to a timestamp which maps to an amount
-// for each address we can see when a deposit is made and the amount
-    mapping(address => mapping(uint => uint)) expandedDepositors;
 
-    uint[] orderOfDeposits;
+    uint[] public orderOfDeposits;
 
     address payable host;
 
-    // modifier onlyOwner() {
-    //     require(msg.sender == host, "You are not the owner.");
-    //     _;
-    // }
+
 
     event Deposit(address indexed _sender, uint _amount);
     event moneyAdded(uint indexed _when, address _sender);
     event Withdraw(uint _amount);
     event Balance(uint _balance);
 
-    constructor() {
+    constructor () payable {
+
         host = payable(msg.sender); // set the owner to deployer's address
+        orderOfDeposits.push(msg.value);
+        depositors[msg.sender] = msg.value;
     }
 
     // function allowing anyone to deposit ether to the contract
     function deposit() public payable {
-        //payable(address(this)).transfer(msg.value);
         depositors[msg.sender] = msg.value;
-        timeDeposited[block.timestamp] = msg.sender; // track when a deposit is made, and by whom
         orderOfDeposits.push(msg.value); // push deposit amount to array
         emit Deposit(msg.sender, msg.value); //emit event with sender and value
         emit moneyAdded(block.timestamp, msg.sender); //emit event with time and msg.sender
@@ -54,11 +48,11 @@ contract PiggyBank is Ownable {
     error insufficientFundsError(address _caller, uint i);
     error indexOutOfRange(address _caller, uint index);
 
-    function withdrawSome(uint _amount, address _to) external payable onlyOwner {
-        if(_amount < address(this).balance){
+    function withdrawSome( address _to) external payable onlyOwner {
+        if(msg.value < address(this).balance){
             revert insufficientFundsError(msg.sender, msg.value);
         }
-        payable(_to).transfer(_amount);
+        payable(_to).transfer(msg.value);
         emit Balance(address(this).balance);
     }
 
@@ -69,7 +63,7 @@ contract PiggyBank is Ownable {
     }
 
     function getOrderOfDeposits(uint _index)public view returns(uint){
-        if(orderOfDeposits[_index] > orderOfDeposits.length -1){
+        if(orderOfDeposits[_index] >= orderOfDeposits.length -1){
             revert indexOutOfRange(msg.sender, _index);
         }else{
             return orderOfDeposits[_index];
